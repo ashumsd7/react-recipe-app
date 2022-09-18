3952872c1fe02c9ab02a7a7d117bc9bb51ed6558
aathym <aat8001@gmail.com>
2022-09-18 22:53:16 +0530

    add search bar and two way binding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
