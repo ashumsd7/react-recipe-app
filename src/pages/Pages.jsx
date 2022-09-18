6e92f170cb90200de387b38c85f8cb853a1273cf
aathym <aat8001@gmail.com>
2022-09-19 01:28:47 +0530

    remove some warnings

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
-import Recipe from "./Recipe";
+import Recipe from "./Recipe.jsx";
 import { Route, Routes, useLocation } from "react-router-dom";
 
 import { AnimatePresence } from "framer-motion";
@@ -15,7 +15,7 @@ function Pages() {
         <Route path="/" element={<Home />} />
         <Route path="/cuisine/:type" element={<Cuisine />} />
         <Route path="/searched/:search" element={<Searched />} />
-        <Route path="/recipe/:id" element={<Recipe />} />
+        <Route path="/recipe/:id" element={<Recipe/>} />
       </Routes>
     </AnimatePresence>
   );
