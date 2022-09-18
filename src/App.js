a820fd09040667f878db6a33252602baaaf503e4
aathym <aat8001@gmail.com>
2022-09-19 00:58:33 +0530

    add logo and navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { GiOpenedFoodCan } from "react-icons/gi";
+import {Link} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiOpenedFoodCan/>
+          <Logo to={"/"}>Delicious</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -13,5 +20,23 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two" cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
