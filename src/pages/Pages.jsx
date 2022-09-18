7bf0aa27591a6c8c6d59c15d70de5b963532be88
aathym <aat8001@gmail.com>
2022-09-18 20:19:02 +0530

    dynamic route params

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -7,7 +7,7 @@ function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cuisine" element={<Cuisine />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
     </Routes>
   );
 }
